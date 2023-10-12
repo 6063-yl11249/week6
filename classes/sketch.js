e5409322e892b7a84e0722af058ace1c672539e0
Yuanqing <yl11249@nyu.edu>
2023-10-11 22:50:03 -0400

    Update sketch.js

--- classes/sketch.js
+++ classes/sketch.js
@@ -1,34 +1,81 @@
-let circles = [];
-let numCircles = 16;
+class Asteroid {
+  constructor() {
+    this.x = 0;
+    this.y = random(30, height - 30);
+    this.v = random(2, 4);
+    this.r = random(15, 40);
+  }
+  update() {
+    this.x += this.v;
+
+    // if reset is needed: pick random diameter and reset x
+    if (this.x > width + this.r) {
+      this.r = random(15, 40);
+      this.x = 0 - this.r;
+    }
+  }
+
+  draw() {
+    fill(220, 10, 100);
+    ellipse(this.x, this.y, 2 * this.r, 2 * this.r);
+  }
+}
+
+class Spaceship {
+  constructor(_color) {
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.vx = random(1, 2);
+    this.vy = random(1, 2);
+    this.color = _color;
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.x > width) {
+      this.x -= width;
+    }
+    if (this.y > height) {
+      this.y -= height;
+    }
+  }
+
+  draw() {
+    noStroke();
+    fill(this.color);
+    rect(this.x, this.y, 50, 15);
+  }
+}
+
+let asteroidArry = [];
+let maxAsteroid = 16;
+
+let spaceship0;
+let spaceship1;
 
 function setup() {
   createCanvas(800, 600);
-  for (let i = 0; i < numCircles; i++) {
-    let myObject = {
-      x: 0,
-      y: random(30, height - 30),
-      v: random(2, 4),
-      r: random(15, 40),
-    };
-    circles.push(myObject);
+  for (let i = 0; i < maxAsteroid; i++) {
+    let anAsteroid = new Asteroid();
+    asteroidArry.push(anAsteroid);
   }
+  spaceship0 = new Spaceship("blue");
+  spaceship1 = new Spaceship("silver");
 }
 
 function draw() {
   background("lightblue");
-  fill(220, 10, 100);
 
-  for (let i = 0; i < circles.length; i += 1) {
-    let myObject = circles[i];
-
-    // draw circles
-    ellipse(myObject.x, myObject.y, 2 * myObject.r, 2 * myObject.r);
-    myObject.x += myObject.v;
-
-    // if reset is needed: pick random diameter and reset x
-    if (myObject.x > width + myObject.r) {
-      myObject.r = random(15, 40);
-      myObject.x = 0 - myObject.r;
-    }
+  for (let i = 0; i < asteroidArry.length; i += 1) {
+    let anAsteroid = asteroidArry[i];
+    anAsteroid.update();
+    anAsteroid.draw();
   }
+  spaceship0.update();
+  spaceship1.update();
+
+  spaceship0.draw();
+  spaceship1.draw();
 }
